30c8a464d7c831f7302cd055d53a64a9ee21d128
Thomas <trich1230@gmail.com>
2020-05-28 00:50:18 -0700

    Updates

---
+++ precache-manifest.2a01c8ea398f7fc576aa79baa73e5dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b567ff26337682c4bbb88a0e6c3dabdd",
+    "url": "/breadbike/index.html"
+  },
+  {
+    "revision": "28d0015f66bea6ce4bad",
+    "url": "/breadbike/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58f8fab3f501813bbcc4",
+    "url": "/breadbike/static/js/2.078f0c46.chunk.js"
+  },
+  {
+    "revision": "550a4af9644562db399dd3701d5f019b",
+    "url": "/breadbike/static/js/2.078f0c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d0015f66bea6ce4bad",
+    "url": "/breadbike/static/js/main.0c73d5a4.chunk.js"
+  },
+  {
+    "revision": "bb42440def5e0eff1439",
+    "url": "/breadbike/static/js/runtime-main.19c51458.js"
+  }
+]);
